4cfb023d10be4d1668bcdd5f41f424440b863996
Vermouth <166023644+vermouth-0404@users.noreply.github.com>
2024-11-08 22:47:07 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
-const responsiveMenu = document.querySelector(".responsive-button");
-responsiveMenu.addEventListener("click", () => {
-    const headerMenu = document.querySelector(".navigation");
-    headerMenu.classList.toggle("menu-active");
-});
+function showMenu() {
+    const responsiveMenu = document.querySelector(".responsive-button");
+    responsiveMenu.addEventListener("click", () => {
+        const headerMenu = document.querySelector(".navigation");
+        headerMenu.classList.toggle("menu-active");
+    });
+};
